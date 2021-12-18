c7c7cb5d084f8af7589d17325bf42c59399a311f
Caleb Martim <calebmartim2113@gmail.com>
2021-12-18 12:34:18 -0300

    ip grabber v2
    Conectado ao Firebase e dados coletados são mandados ao banco de dados Firestore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ip_grabber = require("./abstractapi.js");
+const ip_grabber = require("./geolocationGrabber.js");
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -6,9 +6,6 @@ const PORT = 8080;
 app.get("/", (req, res) => {
   ip_grabber.get_ip();
   res.status(200).sendFile("/index.html", { root: __dirname });
-  //   res.status(200).send({
-  // status: "you done did it",
-  //   });
 });
 
 app.listen(PORT, () => console.log(`It's active on https://localhost:${PORT}`));
